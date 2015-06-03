8b6612278cbf4b9331baa0d266ea0c9f292adc79
Peter <NA>
2015-06-03 23:51:12 +1000

    Output Changes
    Add a "job" directory that a file is written to for each job. The master
    files also now called xml so editors can collapse sections if the
    support it (notepad++)

--- AOLogScanner/ProcDir.cs
+++ AOLogScanner/ProcDir.cs
@@ -15,7 +15,6 @@ namespace AOLogScanner
         SortedList<String, String> FileOrder = new SortedList<String, String>();
         SortedList<String, LineData> JobOrder = new SortedList<String, LineData>();
         string[] AllFiles;
-        StreamWriter sw;
         long LineCounter = 0;      // count the lines, new file will start when needed
         long OutputCounter = 0;    // used for the file name
 
@@ -90,7 +89,12 @@ namespace AOLogScanner
             }
         }
         public void outputData() {
-            sw = new StreamWriter(String.Format(@"{0}\out{1:000}.log", Properties.Settings.Default.OutputLocation, OutputCounter));
+            StreamWriter sw = null; ;
+            StreamWriter job = null; ;
+
+            if (!Directory.Exists(Properties.Settings.Default.OutputLocation + @"\jobs")) {
+                Directory .CreateDirectory(Properties.Settings.Default.OutputLocation + @"\jobs");
+            }
 
             lg.Log("\nTotal Job Lines: " + JobOrder.Count.ToString());
 
@@ -104,35 +108,52 @@ namespace AOLogScanner
             }
 
             LastJob = "xzz";
-            sw.WriteLine(JobOrder.Count);
+//            sw.WriteLine(JobOrder.Count);
             LineCounter = 0;
             foreach (KeyValuePair<string, LineData> kvp in JobOrder) {
                 if (kvp.Value.GetRoot != LastJob) {
+                    if (LineCounter > 0) {
+                        sw.WriteLine("</" + LastJob + ">\n");
+                        job.Close();
+                    }
+                    job = new StreamWriter(String.Format(@"{0}\job-{1}.txt", Properties.Settings.Default.OutputLocation + @"\jobs", FixJobName(kvp.Value.GetRoot)));
                     // start new file on new job
                     // to keep the values together
-                    if (LineCounter > 5000) {
+                    // or is zero so this is the first file
+                    if (LineCounter > 5000 || LineCounter == 0) {
+                        // the file will be open so close ready for the next
+                        if (LineCounter > 0) {
+                            sw.Close();
+                        }
                         LineCounter = 0;
-                        sw.Close();
+                        sw = new StreamWriter(String.Format(@"{0}\out{1:000}.xml", Properties.Settings.Default.OutputLocation, OutputCounter));
                         OutputCounter++;
-                        sw = new StreamWriter(String.Format(@"{0}\out{1:000}.log", Properties.Settings.Default.OutputLocation, OutputCounter));
 
                     }
 
-                    sw.WriteLine("\n" + kvp.Value.GetRoot + " " + kvp.Value.ProcName);
+                    sw.WriteLine("<" + kvp.Value.GetRoot + ">");
+                    sw.WriteLine("" + kvp.Value.GetRoot + " " + kvp.Value.ProcName);
                     sw.WriteLine("  " + kvp.Value.StartType + " Started at " + kvp.Value.StartTime);
+                    job.WriteLine(kvp.Value.ProcName);
+                    job.WriteLine("  " + kvp.Value.StartType + " Started at " + kvp.Value.StartTime);
                     LastJob = kvp.Value.GetRoot;
                 }
                 else {
                     sw.WriteLine("     " + kvp.Value.ProcName + " " + kvp.Value .StartType);
                     for (int y = 0; y < kvp.Value.ExtraOut.Count; y++) {
                         sw.WriteLine("        " + kvp.Value.ExtraOut[y]);
+                        job.WriteLine("        " + kvp.Value.ExtraOut[y]);
                     }
                 }
                 LineCounter++;
             }
 
             sw.Close();
+            job.Close();
         }
-
+        String FixJobName(String Job) {
+            Job = Job.Replace(":", "_");
+            return Job;
+        }
     }
 }
